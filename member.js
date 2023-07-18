17cd3e0fb8dc56cc5b5a330507ba6b360fb513a1
jothigeo <74613277+jothigeo@users.noreply.github.com>
2023-07-18 17:53:33 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/components/skills/member.html',
+        controller: 'SkillsMemberController',
+        controllerAs: 'skillsMemberCtrl',
+        scope: {
+            member: '='
+        }
+    };
+}   
